3849f5f4ba0115de01a885c2665aa9fa6d2968f8
lach0192 <lach0192@algonquinlive.com>
2018-06-20 19:07:39 -0400

    smallEdit
    text changes

--- db.test/public/update.php
+++ db.test/public/update.php
@@ -47,7 +47,7 @@ try {
         <td><?php echo escape($row["age"]); ?></td>
         <td><?php echo escape($row["location"]); ?></td>
         <td><?php echo escape($row["date"]); ?> </td>
-        <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
+        <td><a href="updateSingle.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
       </tr>
     <?php endforeach; ?>
     </tbody>
